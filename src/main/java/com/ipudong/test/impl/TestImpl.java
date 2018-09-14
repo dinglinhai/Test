024fc8c87c9720898f7788a24b80fd1fa3ad2994
丁林芳 <linfang.ding@healthbok.com>
2018-09-14 17:45:22 +0800

    测试

---
+++ src/main/java/com/ipudong/test/impl/TestImpl.java
@@ -0,0 +1,15 @@
+package com.ipudong.test.impl;
+
+import com.ipudong.test.service.Test;
+
+/**
+ * @Author dinglinfang
+ * @Date: 2018/9/14 17:44
+ */
+public class TestImpl implements Test {
+    @Override
+    public String sayHello() {
+        String s = "Hello World!";
+        return s;
+    }
+}
---
+++ src/main/java/com/ipudong/test/service/Test.java
@@ -0,0 +1,9 @@
+package com.ipudong.test.service;
+
+/**
+ * @Author dinglinfang
+ * @Date: 2018/9/14 17:43
+ */
+public interface Test {
+    public String sayHello();
+}
