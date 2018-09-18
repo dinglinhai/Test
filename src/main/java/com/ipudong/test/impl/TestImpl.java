b44e10079b5b424e0096ee31bbe229981542975f
丁林芳 <linfang.ding@healthbok.com>
2018-09-18 10:41:28 +0800

    分支改动

--- src/main/java/com/ipudong/test/impl/TestImpl.java
+++ src/main/java/com/ipudong/test/impl/TestImpl.java
@@ -13,6 +13,11 @@ public class TestImpl implements Test {
         System.out.println(s);
         System.out.println("测试推送");
         System.out.println("测试提交");
+
+        /**
+         * 从dev分支拉下change分支，在change分支上编辑。dev分支也会变化？
+         */
+        System.out.println("测试拉取分支变化");
         return s;
     }
 }
